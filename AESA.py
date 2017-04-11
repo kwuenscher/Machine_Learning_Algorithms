c624a0ffd49b7c59dbcdc002253e5f7f97fb745c
kwuenscher <kwuenscher@users.noreply.github.com>
2017-04-11 23:56:44 +0100

    Update AESA.py

--- AESA.py
+++ AESA.py
@@ -1,7 +1,6 @@
 import numpy as np
 import math
 
-
 class AESA():
     def __init__(self, train_data):
         self.train_data = train_data
@@ -21,40 +20,32 @@ class AESA():
         candidate_pool = list(range(self.data_size[0]))
 
         while candidate_pool:
-
             selected = min(candidate_pool, key=lambda i: lower[i])
-
             current_dist = self.eucDist(q, self.train_data[selected])
 
             best_dist = math.inf
 
             if current_dist < best_dist:
-                best = selected
+                closest = selected
                 best_dist = current_dist
 
             last_candidate_pool = candidate_pool
             candidate_pool = []
 
             for i in last_candidate_pool:
-
                 bounds = abs(current_dist - self.distances[selected][i])
                 lower[i] = max(lower[i], bounds)
-
+                
                 if lower[i] < best_dist:
                     candidate_pool.append(i)
 
-        return self.train_data[best]
-
-
-gaussian_data = np.random.normal(2.4, 0.5, 200).reshape(100, 2)
+        return self.train_data[closest]
 
 if __name__ == '__main__':
     import sys
 
     if 'test' in sys.argv:
-
         gaussian_data = np.random.normal(2.4, 0.5, 200).reshape(100, 2)
-
         aesa = AESA(gaussian_data)
         query = [2,  2]
         print(aesa.getNN(query))
